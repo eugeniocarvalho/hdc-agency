3ebfcf260d3e34f9b27a9b37f04b931800d80f7a
Eugênio Carvalho <eugenio9008@gmail.com>
2020-09-25 10:39:15 -0300

    added parallax to apply-area

--- js/scripts.js
+++ js/scripts.js
@@ -87,5 +87,6 @@ $(function() {
   // Parallax
   setTimeout(function(){
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'})
   }, 250)
 })
